25b2b71b532312697c71e19583cbde523ee66d01
sivertism <sivert.sliper@lyse.net>
2016-02-09 13:14:31 +0100

    Wrote h-file_template.h, fixed names.

---
+++ h-file_template.h
@@ -0,0 +1,25 @@
+/**
+  **************************************************************************************
+  * @file    h-file_template
+  * @author  Sivert Sliper
+  * @version V01
+  * @date    09-February-2016
+  * @brief   This file contains macros and extern function definitions for
+  			 c-file_template.c.
+  **************************************************************************************
+  */
+
+/* Macro -------------------------------------------------------------------------------*/
+/* Put #define's here.
+
+/* ID list, RANGE = [0...0x7FF] *********************************************************/
+
+#define CONSTANT_1							0x01
+#define CONSTANT_2							300
+
+/* Exported function prototypes --------------------------------------------------------*/
+
+/* Declare global functions here, preferrably with comments to explain the functions.
+ */
+
+extern void global_function(void); // Global function that does something.
